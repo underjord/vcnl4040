88055cda5f7b3893264dcf284afc09bd1676a693
Lars Wikman <lars@underjord.io>
2024-02-15 21:17:07 +0100

    Clean up the main module public API

--- lib/vcnl4040.ex
+++ lib/vcnl4040.ex
@@ -13,11 +13,11 @@ defmodule VCNL4040 do
   alias VCNL4040.State
   alias VCNL4040.Hardware
 
-
   @doc """
   Start the ambient light/proximity sensor driver
 
   Options:
+  * `:name` - regular GenServer registration name, makes public API functions more convenient if only using one sensor
   * `:i2c_bus` - defaults to `"i2c-0"`
   * `:device_config` - defaults to turning on ambient light sensor and proximity sensor for polling. Pass your own `DeviceConfig` to modify.
   * `:interrupt_pin` - required to enable interrupt-driven sensing, requires the hardware connection for GPIO INT pin set up
@@ -31,7 +31,8 @@ defmodule VCNL4040 do
   """
   @spec start_link(keyword()) :: GenServer.on_start()
   def start_link(options \\ []) do
-    GenServer.start_link(__MODULE__, options, name: __MODULE__)
+    {gen_opts, options} = Keyword.split(options, [:name])
+    GenServer.start_link(__MODULE__, options, gen_opts)
   end
 
   @impl GenServer
@@ -86,9 +87,7 @@ defmodule VCNL4040 do
     if pin == state.interrupt_pin do
       process_interrupt(timestamp, value, state)
     else
-      Logger.warning(
-        "Received unexpected pin message from GPIO pin #{pin}: #{inspect(value)}"
-      )
+      Logger.warning("Received unexpected pin message from GPIO pin #{pin}: #{inspect(value)}")
 
       {:noreply, state}
     end
@@ -105,30 +104,35 @@ defmodule VCNL4040 do
     {:reply, {:error, :no_sensor}, state}
   end
 
-  def handle_call(:get_ambient_light, _from, %State{} = state) do
+  def handle_call({:get_ambient_light, :filtered}, _from, %State{} = state) do
     {:reply, state.ambient_light.latest_filtered, state}
   end
 
-  def handle_call(:get_ambient_light_lux, _from, %State{} = state) do
+  def handle_call({:get_ambient_light, :lux}, _from, %State{} = state) do
     {:reply, state.ambient_light.latest_lux, state}
   end
 
-  def handle_call(:get_ambient_light_raw, _from, %State{} = state) do
+  def handle_call({:get_ambient_light, :raw}, _from, %State{} = state) do
     {:reply, state.ambient_light.latest_raw, state}
   end
 
-  def handle_call(:get_proximity, _from, %State{} = state) do
+  def handle_call({:get_proximity, :filtered}, _from, %State{} = state) do
     {:reply, state.proximity.latest_filtered, state}
   end
 
-  def handle_call(:get_proximity_raw, _from, %State{} = state) do
+  def handle_call({:get_proximity, :raw}, _from, %State{} = state) do
     {:reply, state.proximity.latest_raw, state}
+  end
+
+  def handle_call(:get_device_config, _from, %State{device_config: dc} = state) do
+    {:reply, dc, state}
   end
 
   def handle_call({:set_device_config, %DeviceConfig{} = device_config}, _from, %State{} = state) do
     device_config
     |> DeviceConfig.get_all_registers_for_i2c()
     |> Hardware.apply_device_config(state.bus_ref)
+
     {:reply, :ok, %State{state | device_config: device_config}}
   end
 
@@ -155,61 +159,60 @@ defmodule VCNL4040 do
     state
   end
 
+  defguard is_server(term) when is_pid(term) or is_atom(term) or is_tuple(term)
+
   @doc """
   Returns true if the combo prox/light sensor is present and matches the expected device ID.
   """
-  @spec sensor_present?() :: boolean
-  def sensor_present? do
-    GenServer.call(__MODULE__, :sensor_present?)
+  @spec sensor_present?(GenServer.server()) :: boolean
+  def sensor_present?(server \\ __MODULE__) when is_server(server) do
+    GenServer.call(server, :sensor_present?)
+  catch
+    :exit, {error, _} ->
+      Logger.error("Failed to check if sensor is present: #{inspect(error)}")
   end
 
+  @light_types [:filtered, :lux, :raw]
   @doc """
   Returns the current filtered reading from the ambient light sensor
 
   Returns `:timeout` or `:noproc` if the GenServer times out or isn't running.
   """
-  @spec get_ambient_light_value :: number() | {:error, :no_sensor | :timeout | :noproc}
-  def get_ambient_light_value do
-    GenServer.call(__MODULE__, :get_ambient_light)
+  @spec get_ambient_light_value(GenServer.server(), :filtered | :lux | :raw) ::
+          number() | {:error, :no_sensor | :timeout | :noproc}
+  def get_ambient_light_value(server \\ __MODULE__, type \\ :filtered)
+      when is_server(server) and type in @light_types do
+    GenServer.call(server, {:get_ambient_light, type})
   catch
     :exit, {error, _} -> {:error, error}
   end
 
-  @spec get_ambient_light_value(:lux) :: number() | {:error, :no_sensor | :timeout | :noproc}
-  def get_ambient_light_value(:lux) do
-    GenServer.call(__MODULE__, :get_ambient_light_lux)
-  catch
-    :exit, {error, _} -> {:error, error}
-  end
-
-  @spec get_ambient_light_value(:raw) :: number() | {:error, :no_sensor | :timeout | :noproc}
-  def get_ambient_light_value(:raw) do
-    GenServer.call(__MODULE__, :get_ambient_light_raw)
-  catch
-    :exit, {error, _} -> {:error, error}
-  end
-
-  def get_proximity_value do
-    GenServer.call(__MODULE__, :get_proximity_value)
-  catch
-    :exit, {error, _} -> {:error, error}
-  end
-
-  def get_proximity_value(:raw) do
-    GenServer.call(__MODULE__, :get_proximity_value_raw)
+  @proximity_types [:filtered, :raw]
+  def get_proximity_value(server \\ __MODULE__, type \\ :filtered)
+      when is_server(server) and type in @proximity_types do
+    GenServer.call(server, {:get_proximity, type})
   catch
     :exit, {error, _} -> {:error, error}
   end
 
   # TODO: Add get_device_config
+  def get_device_config(server \\ __MODULE__) when is_server(server) do
+    GenServer.call(server, :get_device_config)
+  catch
+    :exit, {error, _} -> {:error, error}
+  end
 
-  def set_device_config(%DeviceConfig{} = device_config) do
-    GenServer.call(__MODULE__, {:set_device_config, device_config})
+  def set_device_config(server \\ __MODULE__, %DeviceConfig{} = device_config)
+      when is_server(server) do
+    GenServer.call(server, {:set_device_config, device_config})
+  catch
+    :exit, {error, _} -> {:error, error}
   end
 
   # We use send_after rather than :timer.send_interval to limit the risk of messaging
   # filling up on slow-down
   defp poll_me_maybe(%State{polling_sample_interval: nil}), do: :ok
+
   defp poll_me_maybe(%State{polling_sample_interval: interval}) when interval > 0 do
     Process.send_after(self(), :sample, interval)
   end
