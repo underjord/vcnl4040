b4cde1a11e94cfd3fe6697462e8584014a15693c
Lars Wikman <lars@underjord.io>
2024-02-27 16:29:50 +0100

    Bump to version 0.1.2 for publishing

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule VCNL4040.MixProject do
   def project do
     [
       app: :vcnl4040,
-      version: "0.1.0",
+      version: "0.1.2",
       elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
